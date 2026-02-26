2b14fec4d5e6216f737a793523d41fe70eb605ed
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2026-02-25 21:38:38 -0500

    Update Config, add url direct into ApiProvider

--- src/apis/ApiProvider.ts
+++ src/apis/ApiProvider.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const MODRINTH_URL: string = import.meta.env.VITE_MODRINTH_URL;
-
 export const getModrinthDownloads:(slug: string) => Promise<number> = async (slug: string): Promise<number> => {
-    return (await axios.get(`${MODRINTH_URL}/project/${slug}`)).data.downloads;
+    return (await axios.get(`https://api.modrinth.com/v2/project/${slug}`)).data.downloads;
 };
